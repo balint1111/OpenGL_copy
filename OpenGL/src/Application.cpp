be75e486057a3bff3ad46abce6f4f28f9cae6833
balint1111 <balint.gyorffy@gmail.com>
2020-04-19 21:57:35 +0200

    pixelated scene projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -34,7 +34,7 @@ int main(void)
 	
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -53,10 +53,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f, //0
-			 0.5f, -0.5f, 1.0f, 0.0f, //1
-			 0.5f,  0.5f, 1.0f, 1.0f, //2
-			-0.5f,  0.5f, 0.0f, 1.0f //3
+			 0.0f,  200.0f, 0.0f, 0.0f, //0
+			 200.0f, 200.0f, 1.0f, 0.0f, //1
+			 200.0f,  0.0f, 1.0f, 1.0f, //2
+			 0.0f,  0.0f, 0.0f, 1.0f //3
 
 		};
 
@@ -86,7 +86,7 @@ int main(void)
 		//Indexbuffer bind
 		IndexBuffer ib(indices, Indices_Size);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 540.0f, 0.0f, -1.0f, 1.0f);
 
 		Shader shader("res/shaders/texture.shader");
 		shader.Bind();
