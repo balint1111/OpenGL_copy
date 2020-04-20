48c36f5512579e84112a742b81c4e30f9c6b4fa2
balint1111 <balint.gyorffy@gmail.com>
2020-04-20 17:22:13 +0200

    model view projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -87,15 +87,20 @@ int main(void)
 		IndexBuffer ib(indices, Indices_Size);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 540.0f, 0.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+		glm::mat4 mvp = proj * view * model;
 
 		Shader shader("res/shaders/texture.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		
 
 		Texture texture("res/textures/the_cherno.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		va.UnBind();
 		vb.Unbind();
